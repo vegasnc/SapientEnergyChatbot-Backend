6c1f99af8b65dd011dc02c97a9b1b5a0dd243165
Vegas <vegaslancer1025@gmail.com>
2023-10-03 09:48:09 +0800

    [Update] Implementing emailing feedback feature

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
 from flask import Flask, request
+from flask_mail import Mail, Message
 from pymongo import MongoClient
 from models import keyword_intent # call model file
 from models import questions # call model file
 from models import relevant_api # call model file
 from flask_cors import CORS # to avoid cors error in different frontend like react js or any other
+
 import engine.construct_questions as construct_questions
 import engine.generate_answer as generate_answer
 
@@ -14,6 +16,15 @@ CORS(app, methods=[ 'POST', 'GET' ], allow_headers=[ 'Content-Type' ])
 keyword_intent = keyword_intent.KeywordIntent()
 question_model = questions.Questions()
 relevant_model = relevant_api.RelevantAPI()
+
+# configuration of mail
+app.config['MAIL_SERVER']='sandbox.smtp.mailtrap.io'
+app.config['MAIL_PORT'] = 2525
+app.config['MAIL_USERNAME'] = 'maksymdidkivskyi2@gmail.com'
+app.config['MAIL_PASSWORD'] = 'maksymdidkiv285/'
+app.config['MAIL_USE_TLS'] = False
+app.config['MAIL_USE_SSL'] = True
+mail = Mail(app)
 
 @app.route('/api/get_answer', methods=['POST'])
 def get_answer():
@@ -61,6 +72,30 @@ def get_api_answer():
         return {
             "answer": s_data
         }, 200  
+    
+@app.route('/api/send_feedback', methods=['POST'])
+def get_send_feedback():
+    data = request.get_json()
+    feedback = data["feedback"]
+    history = data["history"]
+    rate_my_data = data["rate_my_data"]
+    rate_my_ability = data["rate_my_ability"]
+
+    msg = Message(
+        'Feedback',
+        sender = 'vegaslancer1025@gmail.com',
+        recipients = ['maksymdidkivskyi2@gmail.com']
+    )
+    msgContent = "ChatHistory : \n\n" + history + "\n\n"
+    msgContent += "Rate My Data : " + str(rate_my_data) + "\n\n"
+    msgContent += "Rate My Ability : " + str(rate_my_ability) + "\n\n"
+    msgContent += "Additional Comments : " + feedback + "\n\n"
+    msg.body = msgContent
+    mail.send(msg)
+
+    return {
+        "result": "success"
+    }, 200
 
 # --------------- Building database --------------- 
 @app.route('/model/question', methods=['GET'])
